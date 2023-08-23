a695f1c0506b1077ae08d382f6a339acffaa3de2
muhdAbdulrahman <mabdul.am25@gmail.com>
2023-08-23 16:40:03 -0700

    errors.c

--- errors.c
+++ errors.c
@@ -1,145 +1,85 @@
 #include "shell.h"
 
 /**
- * _erratoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
- *       -1 on error
- */
-int _erratoi(char *s)
-{
-	int i = 0;
-	unsigned long int result = 0;
-
-	if (*s == '+')
-		s++;  /* TODO: why does this make main return 255? */
-	for (i = 0;  s[i] != '\0'; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-			if (result > INT_MAX)
-				return (-1);
-		}
-		else
-			return (-1);
-	}
-	return (result);
-}
-
-/**
- * print_error - prints an error message to the standard error output.
- * @info: the parameter & return info struct
- * @estr: string containing a specific error type.
- * Return: Returns 0 if no numbers in string, converted numbers otherwise
- *        -1 on error.
- */
-void print_error(info_t *info, char *estr)
-{
-	_eputs(info->fname);
-	_eputs(": ");
-	print_d(info->line_count, STDERR_FILENO);
-	_eputs(": ");
-	_eputs(info->argv[0]);
-	_eputs(": ");
-	_eputs(estr);
-}
-
-/**
- * print_d - function prints a decimal to the specified fd.
- * @input: the integer input
- * @fd: the filedescriptor to write to.
+ * _eputs - output input string value
+ * @str: the string val
  *
- * Return: number of characters printed
+ * Return: Nothing
  */
-int print_d(int input, int fd)
+void _eputs(char *str)
 {
-	int (*__putchar)(char) = _putchar;
-	int i, count = 0;
-	unsigned int _abs_, current;
+int i = 0;
 
-	if (fd == STDERR_FILENO)
-		__putchar = _eputchar;
-	if (input < 0)
-	{
-		_abs_ = -input;
-		__putchar('-');
-		count++;
-	}
-	else
-		_abs_ = input;
-	current = _abs_;
-	for (i = 1000000000; i > 1; i /= 10)
-	{
-		if (_abs_ / i)
-		{
-			__putchar('0' + current / i);
-			count++;
-		}
-		current %= i;
-	}
-	__putchar('0' + current);
-	count++;
-
-	return (count);
+if (!str)
+return;
+while (str[i] != '\0')
+{
+_eputchar(str[i]);
+i++;
+}
 }
 
 /**
- * convert_number - converts a number to a string.
- * @num: number
- * @base: base
- * @flags: argument flags
- *
- * Return: string
- */
-char *convert_number(long int num, int base, int flags)
+* _eputchar - function writes character ch to stderr
+* @ch: character to print
+*
+* Return: On success, return 1.
+* On error, return -1 and set errno appropriately.
+*/
+int _eputchar(char ch)
 {
-	static char *array;
-	static char buffer[50];
-	char sign = 0;
-	char *ptr;
-	unsigned long n = num;
+static int i;
+static char buf[WRITE_BUF_SIZE];
 
-	if (!(flags & CONVERT_UNSIGNED) && num < 0)
-	{
-		n = -num;
-		sign = '-';
-
-	}
-	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
-	ptr = &buffer[49];
-	*ptr = '\0';
-
-	do	{
-		*--ptr = array[n % base];
-		n /= base;
-	} while (n != 0);
-
-	if (sign)
-		*--ptr = sign;
-	return (ptr);
+if (ch == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(2, buf, i);
+i = 0;
+}
+if (ch != BUF_FLUSH)
+buf[i++] = c;
+return (1);
 }
 
 /**
- * remove_comments - function removes comment
- *                   by replacing the first instance of '#' with '\0'.
- * @buffer: address of the string to modify
- *
- * Return: Always 0;
- */
-void remove_comments(char *buffer)
+* _putfd - function that writes the ch to a given fd
+* @ch: The character
+* @fd: The file descriptor to write to
+*
+* Return: On success, return 1.
+* On error, return -1 and set errno appropriately.
+*/
+int _putfd(char ch, int fd)
 {
-	int i = 0;
+static int i;
+static char buf[WRITE_BUF_SIZE];
 
-	while (buffer[i] != '\0')
-	{
-		if (buffer[i] == '#' && (!i || buffer[i - 1] == ' '))
-		{
-			buffer[i] = '\0';
-			break;
-		}
-		i++;
-	}
+if (ch == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(fd, buf, i);
+i = 0;
+}
+if (ch != BUF_FLUSH)
+buf[i++] = ch;
+return (1);
 }
 
+/**
+* _putsfd - function that print an input
+* @str: string
+* @fd: the file descriptor to write to
+*
+* Return: number of chars
+*/
+int _putsfd(char *str, int fd)
+{
+int i = 0;
+
+if (!str)
+return (0);
+while (*str)
+{
+i += _putfd(*str++, fd);
+}
+return (i);
+}
